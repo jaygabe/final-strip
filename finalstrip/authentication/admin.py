c5d68b0e61fd7d7262a84daa44d75224e6cbd84f
justin107d <justin107d@gmail.com>
2022-10-29 00:21:44 -0400

    updated auth admin

--- finalstrip/authentication/admin.py
+++ finalstrip/authentication/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import User
+from .models import Reset, User, UserToken
 
 
 class UserAdmin(BaseUserAdmin):
@@ -34,3 +34,6 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(UserToken)
+admin.site.register(Reset)
