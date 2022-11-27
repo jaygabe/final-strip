9457e8c970d1c00125834b077196929e4e54680d
justin107d <justin107d@gmail.com>
2022-11-27 12:02:14 -0500

    updating tournaments and events apps

--- finalstrip-django/journal_apps/journal/views.py
+++ finalstrip-django/journal_apps/journal/views.py
@@ -1,5 +1,3 @@
-from urllib import response
-from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.db.models import Q
 
@@ -8,18 +6,12 @@ from rest_framework.response import Response
 from rest_framework import exceptions, status
 
 from journal_apps.lessons.models import Lesson
-from journal_apps.usaf_data.models import USAFencingInfo
-from journal_apps.tournaments.models import Tournament
-from journal_apps.events.models import Event
 from journal_apps.bouts.models import Bout
 from journal_apps.fencers.models import Fencer
 
+from .serializers import FencerSerializer, BoutSerializer, LessonSerializer
+from .serializers import AddFencerSerializer, AddBoutSerializer, AddLessonSerializer
 
-from journal_apps.authentication.models import User
-from journal_apps.social.models import ConnectFencers
-from .serializers import FencerSerializer, EventSerializer, BoutSerializer, LessonSerializer, USAFencingSerializer
-from .serializers import AddFencerSerializer, AddEventSerializer, AddBoutSerializer, AddLessonSerializer
-from journal_apps.authentication.authentication import JWTAuthentication
 
 
 
@@ -55,33 +47,6 @@ class FencerView(APIView):
 
             return Response({'Good Request': 'Fencer Added'}, status=status.HTTP_201_CREATED)
         return Response({'Bad Request': 'Invalid Fencer Data...'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class EventView(APIView):
-    # authentication_classes = [JWTAuthentication]
-    serializer_classes = [EventSerializer, AddEventSerializer]
-
-    def get(self, request, slug):
-        if slug:
-            event = get_object_or_404(Event, slug=slug) # user=request.user,
-            bouts = get_list_or_404(Bout, event=event.id)
-            data = {
-                'event': EventSerializer(event).data,
-                'bouts': BoutSerializer(bouts, many=True).data
-            }
-        else:
-            raw_data = get_list_or_404(Event) # user=request.user
-            data = self.serializer_classes[0](raw_data, many=True).data
-
-        return Response(data, status=status.HTTP_200_OK)
-    
-    def post(self, request, slug=None):
-        serializer = self.serializer_classes[1](data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response({'Good Request': 'Event Added'}, status=status.HTTP_201_CREATED)
-        return Response({'Bad Request': 'Invalid Event Data...'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BoutView(APIView):
