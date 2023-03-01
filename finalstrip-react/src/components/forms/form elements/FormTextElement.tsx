53432f552cf875a99eda3186908c3a48351f972b
justin107d <justin107d@gmail.com>
2023-03-01 08:04:00 -0500

    undoing attempt to autofill text field

--- finalstrip-react/src/components/forms/form elements/FormTextElement.tsx
+++ finalstrip-react/src/components/forms/form elements/FormTextElement.tsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 export const FormTextElement = ({ 
     setValue,
     placeholder,
     labelText,
-    elementName
+    elementName,
+    // startingValue
     }:{ 
         setValue: React.Dispatch<React.SetStateAction<string>>,  
         placeholder: string,
         labelText: string,
-        elementName: string
+        elementName: string,
+        // startingValue: string
     }) => {
 
     
     // keep label from covering the input when filled in
     const [className, setClassName]  = useState<string>('')
+    // const [inputValue, setInputValue] = useState<string>(startingValue ? startingValue : '')
+
+    // update the child component if the startingValue changes
+    // useEffect(() => { setInputValue(startingValue ? startingValue : '') }, [startingValue]);
 
     function onChange(entry:string) {
         setValue(entry)
+        // setInputValue(entry)
         if (entry != '') {
             setClassName('jumped')
         }else{
@@ -34,6 +41,7 @@ export const FormTextElement = ({
                 type='text' 
                 placeholder={placeholder}
                 onChange={e => onChange(e.target.value)}
+                // value={inputValue}
             />
             <label className={className} htmlFor='nameInput'>{labelText}</label>
         </div>
